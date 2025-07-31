f49486e5614b623c8a7c957ea5cf88e273fd7b15
Brandon Huang <hotbrandon@hotmail.com>
2025-07-31 15:12:14 +0800

    add channel example

--- channels/main.go
+++ channels/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
+	ch2 := make(chan string, 2)
+
+	go func() {
+		ch2 <- "msg1"
+		ch2 <- "msg2"
+		// This will block if more than 2 messages are sent
+		// until the main thread reads from the channel
+
+		ch2 <- "msg3"
+		fmt.Println("Buffered channel goroutine finished sending messages")
+	}()
+
+	fmt.Println(<-ch2, "and", <-ch2, "plus", <-ch2)
 
 }
