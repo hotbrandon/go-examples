2a336c881db221016c25a6c357b5d24feba6f3a1
Brandon Huang <hotbrandon@hotmail.com>
2025-08-01 17:31:19 +0800

    before applying mutex

---
+++ goroutines/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+type Order struct {
+	ID     int
+	Status string
+	mu     sync.Mutex // Mutex to protect the order status
+}
+
+const (
+	StatusPending    = "pending"
+	StatusProcessing = "processing"
+	StatusCompleted  = "completed"
+)
+
+var OrdersStatuses = []string{StatusPending, StatusProcessing, StatusCompleted}
+
+func GenerateOrders(n int) []*Order {
+	var orders []*Order = make([]*Order, n)
+
+	for i := 0; i < n; i++ {
+		orders[i] = &Order{
+			ID:     i + 1,
+			Status: StatusPending,
+		}
+	}
+
+	return orders
+}
+
+// func ProcessOrders(orders []*Order) {
+// 	for _, order := range orders {
+// 		time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond) // Simulate processing time
+// 		fmt.Printf("Processing Order ID: %d\n", order.ID)
+// 	}
+// }
+
+func UpdateOrders(order *Order) {
+	time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond) // Simulate processing time
+	order.Status = OrdersStatuses[rand.Intn(len(OrdersStatuses))]
+	fmt.Printf("Updating Order ID: %d status to: %s\n", order.ID, order.Status)
+}
+
+func ReportOrderStatuses(orders []*Order) {
+	for _, order := range orders {
+		time.Sleep(1 * time.Second)
+		fmt.Printf("OrderID: %d, Status: %s\n", order.ID, order.Status)
+	}
+}
+
+func main() {
+	wg := sync.WaitGroup{}
+	wg.Add(3) // We will wait for two goroutines
+
+	orders := GenerateOrders(10)
+
+	// go func() {
+	// 	defer wg.Done()
+	// 	ProcessOrders(orders)
+	// }()
+
+	for i := 0; i < 3; i++ {
+		go func() {
+			defer wg.Done()
+			for _, order := range orders {
+				UpdateOrders(order)
+			}
+		}()
+	}
+
+	wg.Wait()
+
+	ReportOrderStatuses(orders)
+}
